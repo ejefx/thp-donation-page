b457177abb75c0354a6618ed16911d2ea0626e10
Aghod <ejedennis07@gmail.com>
2025-08-05 05:13:24 +0100

    Fix: Bind Flask to 0.0.0.0 and dynamic port

--- server.py
+++ server.py
@@ -1,7 +1,9 @@
 from flask import Flask, render_template, request, redirect, jsonify
 import os
 
-app = Flask(__name__)
+if __name__ == '__main__':
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
 
 @app.route('/')
 def index():
